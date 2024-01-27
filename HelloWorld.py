21d084cfb30909135353714695da4799c5272128
Nafis <nafis@aol.com>
2024-01-27 00:55:48 -0500

    First Commit

---
+++ HelloWorld.py
@@ -0,0 +1,21 @@
+
+line01 = "********************"
+line02 = "*                  *"
+line03 = "*      WELCOME     *"
+line04 = "*       FINISH     *"
+
+
+print("")
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+print("")
+print("Hello World")
+print("")
+print(line01)
+print(line02)
+print(line04)
+print(line02)
+print(line01)
